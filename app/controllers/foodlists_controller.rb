515c9af8d3707b2744989bb80a914d6ad13ce551
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-24 15:11:30 +0000

    work on food list

--- app/controllers/foodlists_controller.rb
+++ app/controllers/foodlists_controller.rb
@@ -1,5 +1,33 @@
 class FoodlistsController < ApplicationController
-  def index; end
+  def index
+    @foodlist = Foodlist.all
+  end
 
-  def show; end
+  def show
+    @foodlist = Foodlist.find(params[:id])
+  end
+
+  def destroy
+    @foodlist = Foodlist.find(params[:id])
+    @foodlist.destroy
+    redirect_to foodlists_url, :notice => "Successfully destroyed foodlist."
+  end
+
+  def new
+    @foodlist = Foodlist.new
+  end
+
+  def create
+    @foodlist = Foodlist.new(params[:foodlist])
+    if @foodlist.save
+      redirect_to @foodlist, :notice => "Successfully created foodlist."
+    else
+      render :action => 'new'
+    end
+  end
+
+  private
+  def foodlist_params
+    params.require(:foodlist).permit(:measurement_unit, :price, :quantity)
+  end
 end
