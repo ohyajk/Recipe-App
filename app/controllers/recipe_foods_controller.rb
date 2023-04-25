3cc52536efe7725028d79e32cf9bc62c64c6284e
ohyajk <ohyajk@gmail.com>
2023-04-25 13:41:08 +0530

    Add Recipe list Models

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,5 +1,42 @@
-class RecipeFoodsController < ApplicationController
-  def index; end
+class RecipelistsController < ApplicationController
+  def index
+    @recipelists = Recipelist.all
+  end
 
-  def show; end
+  def show
+    @recipelist = Recipelist.find(params[:id])
+    @recipe_foods = @recipelist.recipe_foods
+  end
+
+  def new
+    @recipelist = Recipelist.new
+  end
+
+  def edit; end
+
+  def create
+    @recipelist = Recipelist.new(recipelist_params)
+
+    if @recipelist.save
+      redirect_to recipelist_path(@recipelist), notice: 'Recipelist was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @recipelist = Recipelist.find(params[:id])
+    @recipelist.destroy
+    redirect_to recipelists_path, notice: 'Recipelist was successfully destroyed.'
+  end
+
+  private
+
+  def set_recipelist
+    @recipelist = Recipelist.find(params[:id])
+  end
+
+  def recipelist_params
+    params.require(:recipelist).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id)
+  end
 end
