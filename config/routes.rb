eaaa834e68634f70127b0d5f9a6c1ae8040c5cc2
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-25 09:29:03 +0000

    update the code

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
   get 'recipe_foods/index'
   get 'recipe_foods/show'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "foodlists#index"
