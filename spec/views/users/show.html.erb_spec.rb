4c65baddb22e151b2a71b99286393625364abbe4
ohyajk <ohyajk@gmail.com>
2023-04-25 13:43:49 +0530

    Fix Linter Errors

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "users/show.html.erb", type: :view do
+RSpec.describe 'users/show.html.erb', type: :view do
   pending "add some examples to (or delete) #{_FILE_}"
 end
