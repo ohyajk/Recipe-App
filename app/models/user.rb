b1cc68b8987d471f1344deed0065f06106710559
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-26 11:27:48 +0000

    update the model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :foodlists
-  has_many :recipelist
+  has_many :foods
+  has_many :recipe
 
   validates :name, presence: true
 end
