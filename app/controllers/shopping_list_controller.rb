76c59c820faeeb78ed7cc13a4b4c7a5b189e164e
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-26 10:42:11 +0000

    fix shopping list bug

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,8 +1,10 @@
 class ShoppingListController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index]
+
   def index
     @user = current_user
-    @recipe = Recipelist.where(user: @user)
-    @ingredients = RecipeFood.where(recipe: @recipe).group_by { |ingredient| ingredient.food.name }
+    @recipelist = Recipelist.where(user: @user)
+    @ingredients = RecipeFood.where(recipelist: @recipelist).group_by { |ingredient| ingredient.food.name }
     @total_price = @ingredients.map do |_food, ingredients|
       ingredients.map do |ingredient|
         ingredient.food.price * ingredient.quantity
