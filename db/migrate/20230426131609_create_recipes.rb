642635628154140659640dc1e717ce969fa34a80
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-26 13:22:58 +0000

    create new migration files

--- db/migrate/20230426131609_create_recipes.rb
+++ db/migrate/20230426131609_create_recipes.rb
@@ -1,12 +1,12 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
-    create_table :recipelists do |t|
-      t.references :user, null: false, foreign_key: true, index: true, to_table: :users
-      t.string :name, limit: 50
+    create_table :recipes do |t|
+      t.string :name
       t.integer :preparation_time
       t.integer :cooking_time
       t.string :description
       t.boolean :public
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20230426131632_create_foods.rb
+++ db/migrate/20230426131632_create_foods.rb
@@ -1,11 +1,11 @@
 class CreateFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :foods do |t|
-      t.references :user, null: false, foreign_key: true, index: true, to_table: :users
-      t.string :name, limit: 50
+      t.string :name
       t.string :measurement_unit
       t.integer :price
       t.integer :quantity
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
