90d51dc287f66a59fa8f4d630151c666949e3053
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-26 12:09:21 +0000

    update migration tables

--- db/migrate/20230424120934_create_foods.rb
+++ db/migrate/20230424120934_create_foods.rb
@@ -1,6 +1,6 @@
-class CreateFoodlists < ActiveRecord::Migration[7.0]
+class CreateFoods < ActiveRecord::Migration[7.0]
   def change
-    create_table :foodlists do |t|
+    create_table :foods do |t|
       t.references :user, null: false, foreign_key: true, index: true, to_table: :users
       t.string :name, limit: 50
       t.string :measurement_unit
--- db/migrate/20230424135818_create_recipes.rb
+++ db/migrate/20230424135818_create_recipes.rb
@@ -1,4 +1,4 @@
-class CreateRecipelists < ActiveRecord::Migration[7.0]
+class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipelists do |t|
       t.references :user, null: false, foreign_key: true, index: true, to_table: :users
