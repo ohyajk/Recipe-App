cc64be337a6b4f98f47b930de029a626960ced1b
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-27 23:53:11 +0000

    Add before_action callbacks, Improve variable assignments, Simplify conditional check, Update redirect path, Improve private method names

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,38 +1,42 @@
 class RecipeFoodsController < ApplicationController
+  before_action :set_recipe, only: [:new, :create]
+  before_action :set_recipe_food, only: [:destroy]
+
   def index
     @recipe_foods = Food.all
   end
 
   def new
-    @recipe_food = RecipeFood.new
-    @recipe_food.recipe_id = params[:recipe_id]
-    @user = User.find(params[:user_id])
+    @recipe_food = @recipe.recipe_foods.new
+    @user = @recipe.user
     @foods = Food.all
   end
 
   def create
-    @recipe = Recipe.find(params[:recipe_id])
     @recipe_food = @recipe.recipe_foods.new(recipe_food_params)
 
-    respond_to do |format|
-      if @recipe_food.save
-        format.html do
-          redirect_to user_recipe_path(user_id: @recipe.user_id, id: @recipe.id),
-                      notice: 'Recipe food was successfully created.'
-        end
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
+    if @recipe_food.save
+      redirect_to user_recipe_path(user_id: @recipe.user_id, id: @recipe.id),
+                  notice: 'Recipe food was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @recipe_food = RecipeFood.find(params[:id])
     @recipe_food.destroy
-    redirect_to user_recipe_path(@recipe_food.recipe_id)
+    redirect_to user_recipe_path(@recipe_food.recipe.user_id, @recipe_food.recipe_id)
   end
 
   private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:recipe_id])
+  end
+
+  def set_recipe_food
+    @recipe_food = RecipeFood.find(params[:id])
+  end
 
   def recipe_food_params
     params.require(:recipe_food).permit(:quantity, :food_id, :recipe_id)
