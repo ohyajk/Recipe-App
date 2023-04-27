731f758db8927ad13a6592cbebe526d157e79081
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-27 23:55:19 +0000

    Add before_action, Improve create action, Update destroy action

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,16 +1,17 @@
 class FoodsController < ApplicationController
+  before_action :set_food, only: [:show, :destroy]
+
   def index
     @foods = Food.all
     @user = current_user
   end
 
   def show
-    @food = Food.find(params[:id])
   end
 
   def destroy
-    @food = Food.find(params[:id])
     @food.destroy
+    redirect_to root_path, notice: 'Food was successfully deleted.'
   end
 
   def new
@@ -18,15 +19,20 @@ class FoodsController < ApplicationController
   end
 
   def create
-    @food = Food.new(food_params)
-    @food.user_id = current_user.id
+    @food = current_user.foods.build(food_params)
 
-    return unless @food.save
-
-    redirect_to root_path, notice: 'Food was successfully created.'
+    if @food.save
+      redirect_to root_path, notice: 'Food was successfully created.'
+    else
+      render :new
+    end
   end
 
   private
+
+  def set_food
+    @food = Food.find(params[:id])
+  end
 
   def food_params
     params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
