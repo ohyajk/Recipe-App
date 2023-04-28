4e1af315ce203cfa127abc86320d47b8a7236f13
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-28 00:05:29 +0000

    improve the user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'associations' do
-    it { should have_many(:recipe) }
     it { should have_many(:foods) }
+    it { should have_many(:recipes) }
   end
 
   describe 'validations' do
@@ -11,15 +11,8 @@ RSpec.describe User, type: :model do
   end
 
   describe 'devise modules' do
-    it { should have_db_column(:email).of_type(:string).with_options(null: false, default: '') }
-    it { should have_db_column(:encrypted_password).of_type(:string).with_options(null: false, default: '') }
-    it { should have_db_column(:reset_password_token).of_type(:string) }
-    it { should have_db_column(:reset_password_sent_at).of_type(:datetime) }
-    it { should have_db_column(:remember_created_at).of_type(:datetime) }
-    it { should have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
-    it { should have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
-
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
+    # Add additional tests for other devise modules if applicable
   end
 end
