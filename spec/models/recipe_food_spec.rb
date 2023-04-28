933ecfe935d2662069df99c45d5721b2b25ef2e0
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-28 00:02:16 +0000

    Add the following: Validations, Instance methods

--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe RecipeFood, type: :model do
     it { should belong_to(:food) }
     it { should belong_to(:recipe) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:quantity) }
+    it { should validate_numericality_of(:quantity).is_greater_than(0) }
+  end
+
+  describe 'instance methods' do
+    let(:recipe_food) { FactoryBot.create(:recipe_food) }
+
+    it 'returns the total calories for the recipe_food' do
+      food = FactoryBot.create(:food, calories: 100)
+      recipe_food.food = food
+      recipe_food.quantity = 2
+      expect(recipe_food.total_calories).to eq(200)
+    end
+  end
 end
